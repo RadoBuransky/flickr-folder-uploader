f93022f3dabd94f82df69190575733eb836b629a
RadoBuransky <radoburansky@gmail.com>
2016-06-09 03:24:46 +0200

    Upload works

--- src/main/scala/com/buransky/flickrFolderUploader/FlickrFolderUploaderApp.scala
+++ src/main/scala/com/buransky/flickrFolderUploader/FlickrFolderUploaderApp.scala
@@ -1,22 +1,34 @@
 package com.buransky.flickrFolderUploader
 
-import java.io.File
+import java.io._
+import javax.swing.filechooser.FileNameExtensionFilter
 
-import com.flickr4java.flickr.auth.{Auth, Permission}
+import com.flickr4java.flickr.auth.Permission
+import com.flickr4java.flickr.photos.SearchParameters
+import com.flickr4java.flickr.photosets.PhotosetsInterface
+import com.flickr4java.flickr.uploader.{UploadMetaData, Uploader}
 import com.flickr4java.flickr.util.FileAuthStore
 import com.flickr4java.flickr.{Flickr, REST, RequestContext}
 import org.scribe.model.Verifier
 
 import scala.collection.JavaConverters._
+import scala.collection.mutable
 import scala.io.StdIn
 
 object FlickrFolderUploaderApp {
+  val fileNameToPhotoId = mutable.HashMap[String, String]()
+  val fileNameToPhotoIdPath = "fileNameToPhotoId.txt"
+  var bufferedWriter: BufferedWriter = _
+
   def main(args: Array[String]): Unit = {
     val userId = args(0)
     val apiKey = args(1)
     val apiSecret = args(2)
+    val rootDir = new File(args(3))
+    println(s"User ID: [$userId]")
     println(s"Flickr API key: [$apiKey]")
     println(s"Flickr API secret: [$apiSecret]")
+    println(s"Root dir: $rootDir")
 
     val flickr = new Flickr(apiKey, apiSecret, new REST())
     val authInterface = flickr.getAuthInterface
@@ -38,16 +50,113 @@ object FlickrFolderUploaderApp {
     }
     RequestContext.getRequestContext.setAuth(auth)
 
-    // Print all albums:
     val photosets = flickr.getPhotosetsInterface
-    println(photosets.getList(userId).getPhotosets.asScala.map(_.getTitle).mkString(","))
+    val uploader = flickr.getUploader
+    val all = photosets.getList(userId).getPhotosets.asScala
+
+    loadFileNameToPhotoIds()
+    bufferedWriter = new BufferedWriter(new FileWriter(new File(fileNameToPhotoIdPath)))
+    try {
+      // Upload all albums
+      rootDir.listFiles().foreach { file =>
+        if (file.isDirectory && !all.exists(_.getTitle == file.getName)) {
+          uploadAlbum(file, uploader, photosets)
+        }
+      }
+    }
+    finally {
+      bufferedWriter.close()
+    }
+
+    // Print all albums:
+//    println(photosets.getList(userId).getPhotosets.asScala.map(_.getTitle).mkString(","))
 
     // Create new album
-    val newAlbum = photosets.create("title", "description", "27549264665")
-    println(newAlbum.getUrl)
+//    val newAlbum = photosets.create("title", "description", "27549264665")
+//    println(newAlbum.getUrl)
   }
 
-  private def authorize(): Unit = {
+  private def loadFileNameToPhotoIds(): Unit = {
+    val file = new File(fileNameToPhotoIdPath)
+    if (file.exists()) {
+      val br = new BufferedReader(new FileReader(file))
+      try {
+        var counter = 0
+        var k = ""
+        do {
+          k = br.readLine()
+          if (k != null) {
+            val v = br.readLine()
+            fileNameToPhotoId.update(k, v)
+            counter += 1
+          }
+        } while (k != null)
 
+        println(s"$counter photo IDs read from file.")
+      }
+      finally {
+        br.close()
+      }
+    }
+  }
+
+  private def uploadAlbum(directory: File,
+                          uploader: Uploader,
+                          photosets: PhotosetsInterface): Unit = {
+    println(s"Uploading album ${directory.getName} ...")
+
+    val fileFilter = new FilenameFilter() {
+      override def accept(dir: File, name: String): Boolean = {
+        val lc = name.toLowerCase
+        lc.endsWith("jpg") || lc.endsWith("jpeg")
+      }
+    }
+
+    // Upload all JPEGs
+    val photoIds = directory.listFiles(fileFilter).toList.map { photoFile =>
+      fileNameToPhotoId.get(photoFile.getAbsolutePath) match {
+        case Some(photoId) =>
+          println(s"Photo already uploaded. Skipping. [${photoFile.getAbsolutePath}]")
+          photoId
+
+        case None =>
+          val metadata = new UploadMetaData()
+          metadata.setFilename(photoFile.getName)
+          metadata.setFilemimetype("image/jpeg")
+          metadata.setTitle(photoFile.getName)
+          metadata.setPublicFlag(false)
+          metadata.setFriendFlag(false)
+          metadata.setFamilyFlag(true)
+          metadata.setHidden(false)
+          metadata.setSafetyLevel("SAFETYLEVEL_SAFE")
+          metadata.setContentType("CONTENTTYPE_PHOTO")
+
+          // Upload synchronously
+          val photoId = uploader.upload(photoFile, metadata)
+          println(s"Photo uploaded. [${photoFile.getName}, $photoId]")
+
+          bufferedWriter.write(photoFile.getAbsolutePath)
+          bufferedWriter.write("\n")
+          bufferedWriter.write(photoId)
+          bufferedWriter.write("\n")
+          bufferedWriter.flush()
+
+          fileNameToPhotoId.update(photoFile.getAbsolutePath, photoId)
+
+          photoId
+      }
+    }
+
+    if (photoIds.nonEmpty) {
+      // Create album
+      val newAlbum = photosets.create(directory.getName, "", photoIds.head)
+      photoIds.tail.foreach { photoId =>
+        photosets.addPhoto(newAlbum.getId, photoId)
+      }
+      println(s"New album created ${newAlbum.getUrl}")
+    }
+    else {
+      println(s"No photos uploaded. Album not created.")
+    }
   }
 }
